6b9d87c9ba80577a9f2004b6f823d377f6d71eac
ronen <narkisr@gmail.com>
2015-09-23 14:52:55 +0300

    supporting jdk 7/8 only and using alteratives

---
+++ manifests/install/alternative.pp
@@ -0,0 +1,16 @@
+# Setting up alterntives for Redhat/Centos
+define jdk::install::alternative(
+  $target
+) {
+  exec{"alternatives --install /usr/bin/${name} ${name} /opt/${target}/bin/${name} 2":
+    user   => root,
+    path   => ['/usr/bin','/bin','/usr/sbin'],
+    unless => "test -f /usr/bin/${name} && /usr/bin/${name} -version",
+  } ->
+
+  exec{"alternatives --set ${name} /opt/${target}/bin/${name}":
+    user   => root,
+    path   => ['/usr/bin','/bin','/usr/sbin'],
+    unless => "/usr/bin/${name} -version",
+  }
+}
--- manifests/install/redhat.pp
+++ manifests/install/redhat.pp
@@ -1,53 +1,50 @@
-# Installing on redhat based systems
+# Installing jdk 7/8 on redhat based systems 
 class jdk::install::redhat {
-  $package = 'jdk-7u51-linux-x64.rpm'
+  $package = $::jdk::version ? {
+    '7'     => 'jdk-7u51-linux-x64.rpm',
+    '8'     => 'jdk-8u60-linux-x64.tar.gz',
+    default => fail("${::jdk::version} not supported!")
+  }
+
+
+  $url = $::jdk::version ? {
+    '7'     => "http://download.oracle.com/otn-pub/java/jdk/7u51-b13/${package}",
+    '8'     => "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/${package}",
+    default => fail("${::jdk::version} not supported!")
+  }
+
+  $flags = '--no-cookies --no-check-certificate'
   $cookie = '"Cookie: oraclelicense=accept-securebackup-cookie"'
 
-  if($::jdk::rpm_url != '') {
-    $url = "${::jdk::rpm_url}/${package}"
-  } else {
-    $url = "http://download.oracle.com/otn-pub/java/jdk/7u51-b13/${package}"
-  }
+  ensure_packages(['wget'])
 
   exec{'download jdk':
-    command  => "wget -O /tmp/${package} --no-cookies --no-check-certificate --header ${cookie} ${url}",
-    user     => 'root',
-    path     => '/usr/bin/',
+    command => "wget -O /tmp/${package} ${flags} --header ${cookie} ${url}",
+    user    => 'root',
+    path    => '/usr/bin/',
     unless  => "/usr/bin/test -f /tmp/${package}",
+    require => Package['wget']
   }
 
-  if($::jdk::version == '6') {
-    exec{'chmod jdk package':
-      command => "chmod +x /tmp/${package}",
-      user    => 'root',
-      path    => '/bin',
-      require => Exec['download jdk']
-    }
-
-    exec{'install jdk':
-      command => "yes \"\" | /tmp/${package}",
-      cwd     => '/tmp',
-      user    => 'root',
-      path    => '/usr/bin/',
-      unless  => '/usr/bin/test -d /usr/java',
-      timeout => 600,
-      require => [Exec['download jdk'], Exec['chmod jdk package']]
-    }
-
-    exec{'update alternative':
-      command => 'alternatives --install /usr/bin/java java /usr/java/jdk1.6.0_38/bin/java 2',
-      user    => 'root',
-      path    => '/usr/sbin/',
-      unless  => '/usr/bin/which java'
-    }
-  } else {
+  if ($::jdk::version == '7') {
     exec{'install jdk':
       command => "/bin/rpm -ivh /tmp/${package}",
       cwd     => '/tmp',
       user    => 'root',
       unless  => '/usr/bin/test -d /usr/java',
       require => Exec['download jdk']
-     }
+    }
+  } elsif ($::jdk::version == '8') {
+    exec{"tar xzf /tmp/${package} -C /opt":
+      user    => root,
+      path    => ['/usr/bin','/bin',],
+      unless  => '/usr/bin/test -d /opt/jdk1.8.0_60/',
+      require => Exec['download jdk']
+    } ->
+    
+    jdk::install::alternative {'javac':
+      target => 'jdk1.8.0_60'
+    }
   }
 
 }
